78685e5f5f6ef536a936f1a823007bc2c8f04326
hrrRafael2 <rafaelhruiz@gmail.com>
2022-04-04 14:37:12 -0500

    version one

---
+++ src/components/shared/AppBanner.vue
@@ -0,0 +1,64 @@
+<script>
+import feather from 'feather-icons';
+
+export default {
+	name: 'Home',
+	data: () => {
+		return {
+			theme: '',
+		};
+	},
+	created() {
+		this.theme = localStorage.getItem('theme') || 'light';
+	},
+	mounted() {
+		feather.replace();
+		this.theme = localStorage.getItem('theme') || 'light';
+	},
+	updated() {
+		feather.replace();
+	},
+	methods: {},
+};
+</script>
+
+<template>
+	<section
+		class="flex flex-col sm:justify-between items-center sm:flex-row mt-12 sm:mt-10"
+	>
+		<!-- Banner left contents -->
+		<div class="w-full md:w-1/3 text-left">
+			<h1
+				class="font-general-semibold text-3xl md:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase"
+			>
+				Hola , somos un equipo de desarrollo de 
+				software
+			</h1>
+			<p
+				class="font-general-medium mt-2 text-lg sm:text-xl  xl:text-2xl text-center sm:text-left leading-none text-gray-400"
+			>
+				Somos un equipo de cuatro personas ,
+				en donde nos gustaria ayudar a su negocio o idea.
+			</p>
+			<div class="flex justify-center sm:block">
+				
+			</div>
+		</div>
+
+		<!-- Banner right illustration -->
+		<div class="w-full md:w-2/3 text-right float-right">
+			<img
+				v-if="theme === 'light'"
+				src="@/assets/images/developer.svg"
+				alt="Developer"
+			/>
+			<img
+				v-else
+				src="@/assets/images/developer-dark.svg"
+				alt="Developer"
+			/>
+		</div>
+	</section>
+</template>
+
+<style scoped></style>
---
+++ src/data/projects.js
@@ -0,0 +1,25 @@
+// Projects data.
+
+const projects = [
+	{
+		id: 1,
+		title: 'Desarrollo de aplicaciones web',
+		category: 'Aplicaciones web',
+		img: require('@/assets/images/web-project-2.jpg'),
+	},
+	{
+		id: 2,
+		title: 'Desarrollo movil',
+		category: 'Aplicacion movil',
+		img: require('@/assets/images/mobile-project-2.jpg'),
+	},
+	{
+		id: 3,
+		title: 'Diseño ui / ux',
+		category: 'UI/UX',
+		img: require('@/assets/images/ui-project-1.jpg'),
+	},
+	
+];
+
+export default projects;
